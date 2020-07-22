c9334e0a2e0bb6684e29ba78b3b1d109b6d45ad5
Roman <valeroman@gmail.com>
2020-07-22 09:13:37 -0500

    docs agregado

---
+++ docs/precache-manifest.ed4075f990c6f71fa117594b0caaea7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4148a20dc722f33093258cded639c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9703a47aa5b99789246",
+    "url": "/static/css/main.d4e61233.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9703a47aa5b99789246",
+    "url": "/static/js/main.10bd2b3e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
